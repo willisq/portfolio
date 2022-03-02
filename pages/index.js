ad29e129a60366124a3bd9cff973d1a0222f2687
william <48537076+willisq@users.noreply.github.com>
2022-03-01 20:34:08 -0400

    filenames fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import About from "../components/About";
 export default function Home() {
